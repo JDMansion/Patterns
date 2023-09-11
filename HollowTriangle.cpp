af5417a5152b635104f73d53630c51ba54253203
Jaideep Sharma <40294495+JDMansion@users.noreply.github.com>
2023-09-11 11:30:19 +0530

    Create HollowTriangle.cpp

---
+++ HollowTriangle.cpp
@@ -0,0 +1,33 @@
+// N=4
+  
+//    * 
+//   * * 
+//  *   * 
+// *     * 
+
+#include <iostream>
+using namespace std;
+
+int main() {
+  int n;
+  cin>>n;
+
+  for(int r=0; r<n; r++) {
+    for(int c=0; c<(n-r-1); c++) {
+      cout<<" ";
+    }
+
+    for(int c=0; c<(r+1); c++) {
+      if(c==0 || c==(r+1-1)){
+        cout<<"* ";
+      }
+      else {
+        cout<<"  ";
+      }
+    }
+    cout<<endl;
+  }
+
+}
+
+  
