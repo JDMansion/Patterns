02eafba260d8091a297903620763fad64af046ad
Jaideep Sharma <40294495+JDMansion@users.noreply.github.com>
2023-09-12 14:53:11 +0530

    Create StarNumericPattern.cpp

---
+++ StarNumericPattern.cpp
@@ -0,0 +1,43 @@
+// N=4
+  
+// 1 
+// 2 * 2 
+// 3 * 3 * 3 
+// 4 * 4 * 4 * 4 
+// 3 * 3 * 3 
+// 2 * 2 
+// 1 
+
+
+#include <iostream>
+using namespace std;
+
+int main() {
+  int n;
+  cin>>n;
+
+  for(int r=0; r<n; r++) {
+    for(int c=0; c<((2*r)+1); c++) {
+      if(c%2 == 0) {
+        cout<<r+1<<" ";
+      }
+      else {
+        cout<<"* ";
+      }
+    }
+    cout<<endl;
+  }
+
+  for(int r=0; r<n; r++) {
+    for(int c=0; c<(n+1-(2*r)); c++) {
+      if(c%2 == 0) {
+        cout<<n-r-1<<" ";
+      }
+      else {
+        cout<<"* ";
+      }
+    }
+    cout<<endl;
+  }
+  
+}
