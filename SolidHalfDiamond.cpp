ae774fc90a2b0402880bd1ae5705363879a11fb5
Jaideep Sharma <40294495+JDMansion@users.noreply.github.com>
2023-09-13 13:59:36 +0530

    Create SolidHalfDiamond.cpp

---
+++ SolidHalfDiamond.cpp
@@ -0,0 +1,32 @@
+// N=4
+  
+// * 
+// * * 
+// * * * 
+// * * * * 
+// * * * 
+// * * 
+// *
+
+#include <iostream>
+using namespace std;
+
+int main() {
+  int n;
+  cin>>n;
+
+  for(int r=0; r<n; r++) {
+    for(int c=0; c<(r+1); c++) {
+      cout<<"* ";
+    }
+    cout<<endl;
+  }
+
+  for(int r=0; r<n-1; r++) {
+    for(int c=0; c<(n-r-1); c++) {
+      cout<<"* ";
+    }
+    cout<<endl;
+  }
+  
+}
